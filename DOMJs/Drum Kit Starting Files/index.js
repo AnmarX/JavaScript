d1cef2efd74de0633567e2418b662004ab409781
AnmarX <anmarbably@gmail.com>
2024-08-11 14:59:49 +0300

    .

--- DOMJs/Drum Kit Starting Files/index.js
+++ DOMJs/Drum Kit Starting Files/index.js
@@ -22,51 +22,51 @@ buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
 
         var buttonText = button.lastElementChild.innerHTML
-        // console.log(buttonText)
+        makeSound(buttonText.toLowerCase())
 
-        switch (buttonText) {
-            case "W".toUpperCase():
-                var audio = new Audio('sounds/kick-bass.mp3');
-                audio.play();
-                break;
-            case "a".toUpperCase():
-                var audio = new Audio('sounds/crash.mp3');
-                audio.play();
-                break;
-            case "s".toUpperCase():
-                var audio = new Audio('sounds/snare.mp3');
-                audio.play();
-                break;
-            case "d".toUpperCase():
-                var audio = new Audio('sounds/tom-1.mp3');
-                audio.play();
-                break;
-            case "j".toUpperCase():
-                var audio = new Audio('sounds/tom-2.mp3');
-                audio.play();
-                break;
-            case "k".toUpperCase():
-                var audio = new Audio('sounds/tom-3.mp3');
-                audio.play();
-                break;
-            case "l".toUpperCase():
-                var audio = new Audio('sounds/tom-4.mp3');
-                audio.play();
-                break;
-
-            default:
-                
-        }
-
-
-      
-
-
-        // alert(`Button ${index + 1} clicked!`);
     });
 });
 
 
+
+document.addEventListener('keypress', (event) => {
+    makeSound(event["key"].toLowerCase())
+})
+
+
+const makeSound = (key) => {
+    switch (key) {
+        case "w".toLowerCase():
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "a".toLowerCase():
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "s".toLowerCase():
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case "d".toLowerCase():
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "j".toLowerCase():
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "k".toLowerCase():
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "l".toLowerCase():
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+
+    }
+}
 
 // for (let i=0;i < buttonsLength;i++) {
 //     buttons[i].addEventListener("click", () => {
