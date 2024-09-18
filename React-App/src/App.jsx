6a486949ec0306b9cfacdcf3c937c9ae2f0b1003
AnmarX <anmarbably@gmail.com>
2024-09-18 15:10:44 +0300

    organizing files with sperating the components and the style

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -1,7 +1,7 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
-import Card from './Card.jsx'
+import Header from './Components/Header.jsx'
+import Footer from './Components/Footer.jsx'
+import Food from './Components/Food.jsx'
+import Card from './Components/Card.jsx'
 
 function App() {
 
--- React-App/src/Components/Card.jsx
+++ React-App/src/Components/Card.jsx
@@ -1,4 +1,4 @@
-import './CSS_Card.css'
+import '../Style/CSS_Card.css'
 
 const Card = () => {
 
