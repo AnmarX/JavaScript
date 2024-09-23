35f284bfa9c0249e24ea11104ee72220c3686b94
AnmarX <anmarbably@gmail.com>
2024-09-24 00:11:39 +0300

    proprety

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Components/Footer.jsx'
 import Food from './Components/Food.jsx'
 import Card from './Components/Card.jsx'
 import Button from './Buttons/Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
 
@@ -10,6 +11,12 @@ function App() {
     <div className='container vh-100'>
 
       <Header></Header>
+
+      <div className='d-flex justify-content-center flex-wrap'>
+        <Student name="nemo" age={30} isStudent={true} ></Student>
+        <Student name="anmar" age={90} isStudent={false} ></Student>
+        <Student name="bordoga" age={10} isStudent={false} ></Student>
+      </div>
 
       <div className='d-flex justify-content-center flex-wrap'>
         <Card></Card>
---
+++ React-App/src/Student.jsx
@@ -0,0 +1,15 @@
+import './Style/CSS_Student.css'
+
+const Student = (props) => {
+    return (
+        <div className="student" >
+            <p>Name: {props.name}</p>
+            <p>age: {props.age}</p>
+            <p>Student: {props.isStudent ? "yes" : "no"}</p>
+        </div>
+
+    )
+
+}
+
+export default Student
