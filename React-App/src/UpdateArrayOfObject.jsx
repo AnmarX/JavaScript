f26b2fce6579d71537364c434653463f3fa7fe09
Anmar <117080591+AnmarX@users.noreply.github.com>
2024-10-26 18:10:23 +0300

    .

--- React-App/src/UpdateArrayOfObject.jsx
+++ React-App/src/UpdateArrayOfObject.jsx
@@ -9,6 +9,10 @@ const ArrayOfObject = () => {
     const [carModel, setCarModel] = useState("")
 
     const handleAddCar = () => {
+        const newCar = { year: carYear, make: carMake, model: carModel }
+        setCars(c => [...c, newCar])
+        setCarMake("")
+        setCarModel("")
 
     }
 
@@ -17,14 +21,16 @@ const ArrayOfObject = () => {
     }
 
     const handleYearChange = (event) => {
-
+        setCarYear(event.target.value)
     }
 
     const handleMakeChange = (event) => {
+        setCarMake(event.target.value)
 
     }
 
     const handleModelChange = (event) => {
+        setCarModel(event.target.value)
 
     }
 
@@ -35,6 +41,18 @@ const ArrayOfObject = () => {
             <ul>
 
             </ul>
+            {/* if you want to make a field readonly just remove the onchange and put the value from react state */}
+            <input type="number" value={carYear} onChange={handleYearChange} /> <br />
+
+            <input type="text" value={carMake} onChange={handleMakeChange}
+                placeholder="Enter Car make" /><br />
+
+            <input type="text" value={carModel} onChange={handleModelChange}
+                placeholder="Enter Car Model" /><br />
+
+            <button onClick={handleAddCar}>Add Car</button>
+
+
         </div>
 
     </>
