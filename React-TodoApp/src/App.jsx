45518a51a29a54662a0918062bdef575e5703684
AnmarX <anmarbably@gmail.com>
2024-11-02 19:26:11 +0300

    .

--- React-TodoApp/src/App.jsx
+++ React-TodoApp/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TodoApp from "./TodoApp"
+
 
 function App() {
 
@@ -9,6 +7,7 @@ function App() {
 
     <>
 
+      <TodoApp></TodoApp>
 
     </>
   )
---
+++ React-TodoApp/src/TodoApp.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+
+const TodoApp = () => {
+
+    return <>
+
+    </>
+}
+
+export default TodoApp
