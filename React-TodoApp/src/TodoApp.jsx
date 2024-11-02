169c1e1c15d6d0a3844b57cc9958ddfc67fecf51
Anmar <117080591+AnmarX@users.noreply.github.com>
2024-11-02 22:20:47 +0300

    .

--- React-TodoApp/src/TodoApp.jsx
+++ React-TodoApp/src/TodoApp.jsx
@@ -2,8 +2,50 @@ import { useState } from 'react'
 
 
 const TodoApp = () => {
+    const [tasks, setTasks] = useState([])
+    const [newTasks, setNewTasks] = useState("")
+
+    const handleInputChange = (event) => {
+        setNewTasks(event.target.value)
+    }
+
+    const addTask = () => {
+
+    }
+
+    const deleteTask = () => {
+
+    }
+
+    const moveTaskUp = () => {
+
+    }
+
+    const moveTaskDown = () => {
+
+    }
+
+
 
     return <>
+        <div className='to-do-list'>
+            <h1>To-Do-List</h1>
+            <div>
+                <input
+                    type="text"
+                    placeholder='enter a task...'
+                    value={newTasks}
+                    onChange={handleInputChange}
+                />
+                <button
+                    className='add-button'
+                    onClick={addTask}>
+                    Add
+                </button>
+            </div>
+
+
+        </div>
 
     </>
 }
