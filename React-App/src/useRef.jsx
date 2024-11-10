ea2b089d5eb152ecb9f85385d6cedf7f380339d5
Anmar <117080591+AnmarX@users.noreply.github.com>
2024-11-10 20:47:59 +0300

    useRef

--- React-App/src/useRef.jsx
+++ React-App/src/useRef.jsx
@@ -12,22 +12,58 @@ import React, { useEffect, useState, useRef } from 'react';
 
 const UseRef = () => {
 
-    let [number, setNumber] = useState(0)
+    // let [number, setNumber] = useState(0)
+    const refInput1 = useRef(null)
+    const refInput2 = useRef(null)
+    const refInput3 = useRef(null)
+
 
     useEffect(() => {
         console.log("re-render")
     })
 
-    const handleClick = () => {
-        setNumber(n => n + 1)
+    const handleClick1 = () => {
+        refInput1.current.focus()
+        refInput1.current.style.backgroundColor = "yellow"
+        refInput2.current.style.backgroundColor = ""
+        refInput3.current.style.backgroundColor = ""
+
 
     }
 
-    return <>
-        <button onClick={handleClick}>
-            click me!
+    const handleClick2 = () => {
+        refInput2.current.focus()
+        refInput1.current.style.backgroundColor = ""
+        refInput2.current.style.backgroundColor = "yellow"
+        refInput3.current.style.backgroundColor = ""
+
+    }
+
+    const handleClick3 = () => {
+        refInput3.current.focus()
+        refInput1.current.style.backgroundColor = ""
+        refInput2.current.style.backgroundColor = ""
+        refInput3.current.style.backgroundColor = "yellow"
+
+    }
+
+    return <div>
+        <button onClick={handleClick1}>
+            click me 1!
         </button>
-    </>
+        <input ref={refInput1} />
+
+        <button onClick={handleClick2}>
+            click me 2!
+        </button>
+        <input ref={refInput2} />
+
+        <button onClick={handleClick3}>
+            click me 3!
+        </button>
+        <input ref={refInput3} />
+    </div>
+
 }
 
 export default UseRef
